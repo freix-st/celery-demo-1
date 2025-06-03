7ac7b7b0fa075447ea6313e9d8dad7e570c088c8
doubleskeleton <xgoliath@protonmail.com>
2025-06-04 00:20:28 +0800

    Added calling external script using subprocess

---
+++ external_file.py
@@ -0,0 +1,5 @@
+def main():
+    print("This represents an external script to run")
+
+if __name__ == "__main__":
+    main()
--- tasks.py
+++ tasks.py
@@ -1,8 +1,18 @@
+import subprocess
+import sys
 from celery_app import app
 
 @app.task
 def run_daily_script():
-    print("[SUCCESS] Running daily script!")
+    # run script as subprocess
+    try:
+        result = subprocess.run([
+            sys.executable, './external_file.py'
+        ], capture_output=True, text=True, check=True)
+        print("[SUCCESS] Running daily script!")
+    except subprocess.CalledProcessError as e:
+        print(f"[FAILED] {e.stderr}")
+
 
 @app.task
 def generate_report():
